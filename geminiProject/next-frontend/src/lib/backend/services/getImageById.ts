8c6610645fc15e1f190db7963c9ca753dc1c5cb3
Shubhankar <shuboyjee1@gmail.com>
2025-09-14 13:43:20 +0530

    fix: globalCache-import-fix

--- geminiProject/next-frontend/src/lib/backend/services/getImageById.ts
+++ geminiProject/next-frontend/src/lib/backend/services/getImageById.ts
@@ -1,6 +1,7 @@
-import { imageCache } from "../controllers/gemini.controller";
+import { imageCache } from "../utils/imageCache";
 
 export const getImageById = async ({ imageId }: { imageId: string }) => {
   const imageDataSrc = imageCache.get(imageId);
+
   return imageDataSrc;
 };
