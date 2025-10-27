723c8c32cc4bcd5bdfbd9b50a36437fc52ab0dc9
Shubhankar <shuboyjee1@gmail.com>
2025-10-27 18:41:17 +0530

    deleted redundant backend dir

--- geminiProject/next-frontend/src/lib/backend/services/getImageById.ts
+++
@@ -1,7 +0,0 @@
-import { imageCache } from "../utils/imageCache";
-
-export const getImageById = async ({ imageId }: { imageId: string }) => {
-  const imageDataSrc = imageCache.get(imageId);
-
-  return imageDataSrc;
-};
